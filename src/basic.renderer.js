432afe99175de4a1aab98069c70d8e6eaa39c984
tomazcuber <tomazcuber@outlook.com>
2021-04-17 19:06:40 -0300

    Commit final. Adiciona método inside para triangulos, método de transformação afim, triangulação de círculos e polígonos convexos usando fan triagulation

--- src/basic.renderer.js
+++ src/basic.renderer.js
@@ -11,9 +11,25 @@
         
     function inside(  x, y, primitive  ) {
             // You should implement your inside test here for all shapes   
-            // for now, it only returns a false test
+
+            let isInside = false;
+            for(let i = 0; i < primitive.vertices.length; i++) {
+                let v1 = primitive.vertices[i % primitive.vertices.length]
+                let v2 = primitive.vertices[(i+1) % primitive.vertices.length]
+                let edge = [v2[0] - v1[0], v2[1] - v1[1]];
+                let normal =  [edge[1], -edge[0]];
+                let distanceVector = [x - v1[0], y - v1[1]];
+                let dotProduct = normal[0] * distanceVector[0] + normal[1] * distanceVector[1];
+                if(dotProduct > 0){
+                     isInside = false;
+                     break;
+                     console.log("point: (",x,", ",y,"is not inside" );
+                }
+                else isInside = true;
+            }
+
+            return isInside;
             
-            return false
     }
         
     
@@ -29,13 +45,21 @@
             boundingbox: function(primitive) {
                 let bbmin = [Infinity, Infinity];
                 let bbmax = [-Infinity, -Infinity];
-                for(var vertice of primitive.vertices){
-                    if(vertice[0] < bbmin[0]) bbmin[0] = vertice[0];
-                    if(vertice[1] < bbmin[1]) bbmin[1] = vertice[1];
-                    if(vertice[0] > bbmax[0]) bbmax[0] = vertice[0];
-                    if(vertice[1] > bbmax[1]) bbmax[1] = vertice[1];
-                }
-
+                // if(primitive.shape != "circle"){
+                    for(var vertice of primitive.vertices){
+                        if(vertice[0] < bbmin[0]) bbmin[0] = vertice[0];
+                        if(vertice[1] < bbmin[1]) bbmin[1] = vertice[1];
+                        if(vertice[0] > bbmax[0]) bbmax[0] = vertice[0];
+                        if(vertice[1] > bbmax[1]) bbmax[1] = vertice[1];
+                    }
+                // } else {
+                    // bbmin = primitive.center.map((value) => {return value - primitive.radius});
+                    // bbmax = primitive.center.map((value) => {return value + primitive.radius});
+                // }
+                bbmin[0] = Math.floor(Math.min(bbmin[0], vertice[0]));
+                bbmin[1] = Math.floor(Math.min(bbmin[1], vertice[1]));
+                bbmax[0] = Math.ceil(Math.max(bbmax[0], vertice[0]));
+                bbmax[1] = Math.ceil(Math.max(bbmax[1], vertice[1]));
                 return [bbmin, bbmax];
             },
 
@@ -53,28 +77,104 @@
                 vertices[index] = [transformedVertice[0], transformedVertice[1]];
             },
 
+            circleTriangulation: function(circle, nVertices){
+                let generatedVertices = [];
+                let angle = (2 * Math.PI) / nVertices;
+                for(let i = 0; i < nVertices; i++){
+                    let x = circle.radius * Math.cos(i * angle) + circle.center[0];
+                    let y = circle.radius * Math.sin(i * angle) + circle.center[1];
+                    generatedVertices.push([x,y]);
+                    
+                }
+
+                let generatedTriangles = [];
+                const Triangle = class {
+                    constructor(vertices){
+                        this.shape = "triangle";
+                        this.vertices = vertices;
+                        this.color = circle.color;
+                        if(circle.hasOwnProperty("xform")){
+                            this.xform = circle.xform;
+                        }
+                    }
+                    
+                };
+                for(let i = 0; i < generatedVertices.length; i++){
+                    console.log("Triangle ", i, " = [(", circle.center, ", ", generatedVertices[(i % nVertices)], ", ", generatedVertices[((i + 1) % nVertices)], ")" )
+                    let triangle = new Triangle([circle.center, generatedVertices[(i % nVertices)], generatedVertices[((i + 1) % nVertices)]]);
+                    generatedTriangles.push(triangle)
+                    console.log(triangle)
+                }
+                
+                return generatedTriangles;
+                
+            },
+
+            fanTriangulation(polygon){
+                let generatedTriangles = [];
+                const Triangle = class {
+                    constructor(vertices){
+                        this.shape = "triangle";
+                        this.vertices = vertices;
+                        this.color = polygon.color;
+                        if(polygon.hasOwnProperty("xform")){
+                            this.xform = polygon.xform;
+                        }
+                    }
+                    
+                };
+                let nVertices = polygon.vertices.length;
+                for(let i = 1; i < nVertices - 1; i++){
+                    let triangle = new Triangle([polygon.vertices[0], polygon.vertices[i % nVertices], polygon.vertices[(i+1) % nVertices]]);
+                    generatedTriangles.push(triangle);
+                    //console.log(triangle);
+                }
+                //console.log(generatedTriangles)
+                return generatedTriangles;
+            },
+
             preprocess: function(scene) {
                 // Possible preprocessing with scene primitives, for now we don't change anything
                 // You may define bounding boxes, convert shapes, etc
                 
                 var preprop_scene = [];
 
-                for( var primitive of scene ) {  
-                    // do some processing
-                    if(primitive.hasOwnProperty("xform")){
-                       primitive.vertices.forEach(this.applyXForm, primitive.xform);
-                       console.log("Vertices are now: ", primitive.vertices);
+                for( var primitive of scene ) { 
+                    //console.log(primitive) 
+                    
+                    if(primitive.shape == "circle"){
+                        let circleTriangles = this.circleTriangulation(primitive, 30);
+                        for(var triangle of circleTriangles){
+                          scene.push(triangle);
+                        }
+                        continue;  
+                    }
+
+                    if(primitive.shape == "polygon"){
+                        let polygonTriangles = this.fanTriangulation(primitive);
+                        for(var triangle of polygonTriangles){
+                            scene.push(triangle);
+                        }
+                        //scene.pop();
+                        continue;
                     }
                     
-                    let boundingbox = this.boundingbox(primitive)
-                    console.log("Generated Bounding Box:\n\tBB_min: " + boundingbox[0] + "\n\tBB_max: " + boundingbox[1] + "\n");
-                    primitive.boundingbox = boundingbox;
                     
+                    if(primitive.hasOwnProperty("xform")){
+                       primitive.vertices.forEach(this.applyXForm, primitive.xform);
+                    }
 
+                    
+                    
+                    let boundingbox = this.boundingbox(primitive)
+                    primitive.boundingbox = boundingbox;
+                    console.log(primitive.boundingbox)
+                    
+                    
                     preprop_scene.push( primitive );
                     
                 }
-
+                console.log(preprop_scene)
                 
                 return preprop_scene;
             },
